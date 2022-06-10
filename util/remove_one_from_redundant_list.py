c122882422957190cb7b91d3c0142f00839d42d3
Jinyamyzk <63580543+Jinyamyzk@users.noreply.github.com>
2022-06-10 18:17:32 +0900

    add util

---
+++ util/remove_one_from_redundant_list.py
@@ -0,0 +1,17 @@
+import re
+
+
+def remove_one(redundant_list:list[int], remove_number:int) -> list[int]:
+    not_removed = True
+    ret = []
+    for l in redundant_list:
+        if l == remove_number and not_removed:
+            not_removed = False
+            continue
+        ret.append(l)
+    return ret
+
+if __name__ == "__main__":
+    redundant_list = [1,1,2,3,3,3,4]
+    remove_number = 3
+    print(remove_one(redundant_list, remove_number))
